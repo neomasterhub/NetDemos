d08eed59495c61cc9b54da72f5823625a915c833
Neomaster <the@neomaster.net>
2022-12-22 23:19:37 +0300

    Threads/State

---
+++ Threads/State/Program.cs
@@ -0,0 +1,28 @@
+﻿static void WriteInfo(Thread th)
+{
+    Console.WriteLine("id: " + th.ManagedThreadId);
+    Console.WriteLine("name: " + th.Name);
+    Console.WriteLine();
+}
+
+static void MyThread()
+{
+    Thread.CurrentThread.Name = "My Thread";
+}
+
+Thread.CurrentThread.Name = "Main";
+WriteInfo(Thread.CurrentThread);
+
+var th = new Thread(MyThread);
+th.Start();
+Thread.Sleep(100);
+
+WriteInfo(th);
+
+Console.ReadKey();
+
+//id: 1
+//name: Main
+
+//id: 9
+//name: My Thread
