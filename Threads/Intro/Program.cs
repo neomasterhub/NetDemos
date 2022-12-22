94e5c66fe6ad9efb9e922f0c3923bc22e24c3951
Neomaster <the@neomaster.net>
2022-12-22 22:54:09 +0300

    Add project files.

---
+++ Threads/Intro/Program.cs
@@ -0,0 +1,17 @@
+﻿using Intro;
+
+System.Threading.Thread thread1 = new(Threads.StarWriter);
+System.Threading.Thread thread2 = new(new ParameterizedThreadStart(Threads.Writer));
+
+thread1.Start();
+thread2.Start('@');
+
+int i = 30;
+while (i-- > 0)
+{
+    Console.Write('.');
+    Thread.Sleep(500);
+}
+
+// Output:
+// *.@...*....*....*..@..*....*....*....@*..***@***@****@***@***
---
+++ Threads/Intro/Threads.cs
@@ -0,0 +1,23 @@
+﻿namespace Intro;
+
+internal static class Threads
+{
+    public static void StarWriter()
+    {
+        while (true)
+        {
+            Console.Write('*');
+            Thread.Sleep(2000);
+        }
+    }
+
+    public static void Writer(object value)
+    {
+        while (true)
+        {
+            Console.Write(value);
+            Thread.Sleep(7000);
+        }
+    }
+}
+
