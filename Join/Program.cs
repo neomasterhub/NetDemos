db14823819b860ffd7fc8eaed6cc9ef34a199122
Neomaster <the@neomaster.net>
2022-12-23 18:03:01 +0300

    Threads/Join

---
+++ Join/Program.cs
@@ -0,0 +1,10 @@
+﻿using Join;
+
+Threads.Thread1.Start();
+Threads.Thread2.Start();
+
+Threads.Thread2.Join();
+Threads.For('.');
+
+Console.ReadKey();
+//*****+++++.....
---
+++ Join/Threads.cs
@@ -0,0 +1,27 @@
+﻿namespace Join;
+
+internal class Threads
+{
+    public static Thread Thread1 = new(Th1);
+    public static Thread Thread2 = new(Th2);
+
+    public static void For(char symbol)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Thread.Sleep(100);
+            Console.Write(symbol);
+        }
+    }
+
+    private static void Th1()
+    {
+        For('*');
+    }
+
+    private static void Th2()
+    {
+        Thread1.Join();
+        For('+');
+    }
+}
