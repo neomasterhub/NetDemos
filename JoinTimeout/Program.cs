0eef5ae9535af5e889a9df632d51eb719e33377b
Neomaster <the@neomaster.net>
2022-12-23 18:05:37 +0300

    Timeout/JoinTimeout

---
+++ JoinTimeout/Program.cs
@@ -0,0 +1,24 @@
+﻿static void For(char c)
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        Thread.Sleep(200);
+        Console.Write(c);
+    }
+}
+
+Thread th1 = new(() => For('1'));
+Thread th2 = new(() =>
+{
+    th1.Join(TimeSpan.FromSeconds(5));
+    For('2');
+});
+Thread th3 = new(() =>
+{
+    th2.Join(2000);
+    For('3');
+});
+
+th1.Start();
+th2.Start();
+th3.Start();
