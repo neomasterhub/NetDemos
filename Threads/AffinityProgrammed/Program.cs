cf77c819117697df6f4233c9c1759b03a339f0d9
Neomaster <the@neomaster.net>
2022-12-23 19:56:56 +0300

    Threads/AffinityProgrammed

---
+++ Threads/AffinityProgrammed/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace AffinityProgrammed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(15));
+
+            // CPU3 | CPU2 | CPU1 | CPU0
+            // off  | on   | on   | off   <-> 0110 = 6(dec)
+            Process.GetCurrentProcess().ProcessorAffinity = (IntPtr)6;
+
+            while (true) ;
+        }
+
+    }
+}
