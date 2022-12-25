efc92dbb0570d39373ca5f9f556e687da6ef446d
Neomaster <the@neomaster.net>
2022-12-25 16:05:54 +0300

    Threads.Sync/Lock

---
+++ Threads.Sync/Lock/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace Lock;
+
+class Program
+{
+    private static readonly object locked = new();
+    private static readonly StringBuilder resource = new();
+
+    private static void Writer(char c)
+    {
+        lock (locked)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                resource.Append(c);
+                Thread.Sleep(100);
+            }
+
+            resource.Append('\n');
+        }
+    }
+
+    private static void Main()
+    {
+        List<Thread> threads = new();
+
+        for (var i = 'A'; i < 'F'; i++)
+        {
+            char c = i;
+            threads.Add(new Thread(() => Writer(c)));
+        }
+
+        threads.ForEach(th =>
+        {
+            th.Start();
+            th.Join();
+            Thread.Sleep(10);
+        });
+
+        Console.WriteLine(resource.ToString());
+
+        //AAAAAAAAAA
+        //BBBBBBBBBB
+        //CCCCCCCCCC
+        //DDDDDDDDDD
+        //EEEEEEEEEE
+    }
+}
